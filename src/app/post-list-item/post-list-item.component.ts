a1cf10ddf3db3018b38f382eb8f60b4c58dfad07
DidiDev <sans.elodie@dididev.fr>
2018-05-31 12:35:27 +0200

    pour toi Open Classroom

---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.css']
+})
+export class PostListItemComponent implements OnInit {
+
+  @Input() itemName: string;
+  @Input() itemDescription: string;
+  @Input() love = 0;
+
+  isAuth = false;
+
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 1000
+    );
+  }
+
+  ngOnInit() {
+  }
+
+  onLove() {
+    console.log('yesyes');
+    return this.love++;
+  }
+
+  offLove() {
+    console.log('nono');
+    return this.love--;
+  }
+}
