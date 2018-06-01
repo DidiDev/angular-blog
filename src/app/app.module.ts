880df38fb67adf4beceafe577ff2870adf9683ee
DidiDev <sans.elodie@dididev.fr>
2018-06-01 10:14:45 +0200

    ajout du bouton pour tout supprimer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
 import { FormsModule } from '@angular/forms';
+import { PostService } from './services/post.service';
+import { NewPostComponent } from './new-post/new-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostListItemComponent
+    PostListItemComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    PostService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,13 @@
+// Ce service à pour objectif de stocker l'array des post et emettra sous forme de Subject
+// Permet aussi l'ajout d'un nouveau post et la supprétion d'un post existant.
+
+
+export class PostService {
+
+  constructor() { }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+  }
+
+}
